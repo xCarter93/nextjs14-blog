e1902e7035c259caf991701f52587fe0c3e8d753
Patrick Carter <xCarter93@gmail.com>
2024-01-08 16:17:40 -0500

    updating auth route.ts for Github login.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,18 @@
-export { GET, POST } from "@/lib/auth/auth";
+// export { GET, POST } from "@/lib/auth/auth";
+
+import { env } from "@/lib/env";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_SECRET_ID,
+    }),
+  ],
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
